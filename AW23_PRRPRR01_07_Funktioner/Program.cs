d7ccb775377fa6d60afe983f435d2f87d575f70c
magisterblaman <hannes.blaman@lbs.se>
2024-09-12 12:05:54 +0200

    Funktioner

--- AW23_PRRPRR01_07_Funktioner/Program.cs
+++ AW23_PRRPRR01_07_Funktioner/Program.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace AW23_PRRPRR01_07_Funktioner {
 	internal class Program {
+
+		static void PrintGreeting() {
+			Console.WriteLine("Tjena tjena");
+		}
+
+		static string GetGreeting() {
+			return "Tjena tjena";
+		}
+
+		static void PrintGreetingToPerson(string name) {
+			Console.WriteLine("Hejhej " + name);
+		}
+
+		static int Double(int x) {
+			return x * 2;
+		}
+
 		static void Main(string[] args) {
+			PrintGreeting();
+
+			PrintGreetingToPerson("Anna");
+			PrintGreetingToPerson("Sigge");
+
+			int doubled = Double(2);
+			Console.WriteLine(doubled);
+
+			string greeting = GetGreeting();
+
+			Console.WriteLine(greeting);
 		}
 	}
 }
